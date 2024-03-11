2119828053c4148df83c30ed3e9c6a9dc87c2049
Antoinne Bryce Salvador <antoinne_salvador@dlsu.edu.ph>
2024-03-11 17:43:00 +0800

    update to accept string, comment out hydrogen code

--- DistributedH20Simulation-Server/DistributedH20Simulation-Server.cpp
+++ DistributedH20Simulation-Server/DistributedH20Simulation-Server.cpp
@@ -32,13 +32,13 @@ int main() {
         return 1;
     }
 
-    // Create a socket for clients
-    SOCKET hClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (hClientSocket == INVALID_SOCKET) {
-        std::cout << "Hydrogen client socket creation failed." << std::endl;
-        WSACleanup();
-        return 1;
-    }
+    //// Create a socket for clients
+    //SOCKET hClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    //if (hClientSocket == INVALID_SOCKET) {
+    //    std::cout << "Hydrogen client socket creation failed." << std::endl;
+    //    WSACleanup();
+    //    return 1;
+    //}
 
     // Bind the client socket to an address and port
     sockaddr_in oClientAddr;
@@ -54,18 +54,18 @@ int main() {
     }
 
 
-    // Bind the client socket to an address and port
-    sockaddr_in hClientAddr;
-    hClientAddr.sin_family = AF_INET;
-    hClientAddr.sin_port = htons(5000); // Port number
-    hClientAddr.sin_addr.s_addr = INADDR_ANY; // Accept connections from any address
+    //// Bind the client socket to an address and port
+    //sockaddr_in hClientAddr;
+    //hClientAddr.sin_family = AF_INET;
+    //hClientAddr.sin_port = htons(5000); // Port number
+    //hClientAddr.sin_addr.s_addr = INADDR_ANY; // Accept connections from any address
 
-    if (bind(hClientSocket, reinterpret_cast<sockaddr*>(&hClientAddr), sizeof(hClientAddr)) == SOCKET_ERROR) {
-        std::cout << "Hydrogen client socket bind failed." << std::endl;
-        closesocket(hClientSocket);
-        WSACleanup();
-        return 1;
-    }
+    //if (bind(hClientSocket, reinterpret_cast<sockaddr*>(&hClientAddr), sizeof(hClientAddr)) == SOCKET_ERROR) {
+    //    std::cout << "Hydrogen client socket bind failed." << std::endl;
+    //    closesocket(hClientSocket);
+    //    WSACleanup();
+    //    return 1;
+    //}
 
     // Listen for incoming connections on the client socket
     if (listen(oClientSocket, SOMAXCONN) == SOCKET_ERROR) {
@@ -75,52 +75,63 @@ int main() {
         return 1;
     }
 
-    // Listen for incoming connections on the slave socket
-    if (listen(hClientSocket, SOMAXCONN) == SOCKET_ERROR) {
-        std::cout << "Hydrogen socket listen failed." << std::endl;
-        closesocket(hClientSocket);
+    //// Listen for incoming connections on the slave socket
+    //if (listen(hClientSocket, SOMAXCONN) == SOCKET_ERROR) {
+    //    std::cout << "Hydrogen socket listen failed." << std::endl;
+    //    closesocket(hClientSocket);
+    //    WSACleanup();
+    //    return 1;
+    //}
+
+    std::cout << "Server is running..." << std::endl;
+
+    SOCKET o_client_socket;
+    sockaddr_in o_clientAddr;
+    int o_clientAddrSize = sizeof(o_clientAddr);
+    o_client_socket = accept(oClientSocket, reinterpret_cast<sockaddr*>(&o_clientAddr), &o_clientAddrSize);
+    if (o_client_socket == INVALID_SOCKET) {
+        std::cerr << "Accept failed.\n";
+        closesocket(oClientSocket);
         WSACleanup();
         return 1;
     }
 
-    std::cout << "Server is running..." << std::endl;
+    //SOCKET h_client_socket;
 
 
-
-
-
-
-
-    while (true) {
-        SOCKET o_client_socket = accept(oClientSocket, NULL, NULL);
-        if (o_client_socket == INVALID_SOCKET) {
-            std::cerr << "Error accepting connection" << std::endl;
-        }
-        SOCKET h_client_socket = accept(hClientSocket, NULL, NULL);
+    /*SOCKET h_client_socket = accept(hClientSocket, NULL, NULL);
         if (h_client_socket == INVALID_SOCKET) {
             std::cerr << "Error accepting connection" << std::endl;
-        }
-        std::string o_molecule;
-        // receive a string from the o client
-        const int bufferSize = 1024;
-        char buffer[bufferSize];
-        int bytesReceived = recv(o_client_socket, buffer, bufferSize, 0);
-        if (bytesReceived == SOCKET_ERROR) {
-            std::cerr << "Error receiving data" << std::endl;
-        }
-        std::istringstream iss(std::string(buffer, bytesReceived));
-        Log receivedLog;
-        {
-            cereal::BinaryInputArchive archive(iss);
-            archive(receivedLog);
-        }
-        std::cout << "Received: " << receivedLog.id << " " << receivedLog.type << std::endl;
-        // clear buffer
-        memset(buffer, 0, bufferSize);
-    }
+        }*/
 
+    const int bufferSize = 12;
+    char buffer[bufferSize];
+    int bytesReceived;
+
+
+    do {
+        bytesReceived = recv(o_client_socket, buffer, bufferSize, 0);
+        std::cout << bytesReceived;
+        if (bytesReceived > 0) {
+
+            /*std::string strBuffer(buffer);
+            std::stringstream ss(strBuffer);
+            std::string id, type, timestamp;
+            ss >> id >> type;*/
+
+            std::cout << "Received: " << std::string(buffer, bytesReceived) << std::endl;
+        }
+        else if (bytesReceived == 0) {
+            std::cout << "Connection closed by peer.\n";
+        }
+        else {
+            std::cerr << "Receive failed.\n";
+        }
+    } while (bytesReceived > 0);
+
+    closesocket(o_client_socket);
     closesocket(oClientSocket);
-    closesocket(hClientSocket);
+    //closesocket(hClientSocket);
     WSACleanup();
     return 0;
 }
